7f769fe7d1e27f7b9a76562a7d6c840ee86a8d9a
Stan-T-Pet <114077038+Stan-T-Pet@users.noreply.github.com>
2025-03-28 14:43:30 +0000

    lesson

--- src/pages/child/lessons.js
+++ src/pages/child/lessons.js
@@ -6,104 +6,6 @@ import Link from "next/link";
 import connectDB from "../../server/config/database";
 import Lesson from "../../server/models/Lesson";
 
-<<<<<<< HEAD
-export default function Lessons() {
-    return (
-        <Box>
-            {/* AppBar */}
-            <AppBar position="static" sx={{ width: '100%' }}>
-                <Toolbar>
-                    <Typography 
-                        variant="h6" 
-                        component="div" 
-                        sx={{ flexGrow: 1, textAlign: 'center' }}
-                    >
-                        Welcome, {"Guest"}!
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-
-            {/* Main Content */}
-            <Container>
-                <Box
-                    sx={{
-                        display: 'grid',
-                        gridTemplateColumns: '1fr auto 1fr',
-                        alignItems: 'center',
-                        gap: 2,
-                        marginTop: 2,
-                    }}
-                >
-                    {/* Left Section */}
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Typography
-                            variant="h4"
-                            sx={{ textAlign: 'center' }}
-                        >
-                            Select which lesson to do below:
-                        </Typography>
-                        <Button>Go Back</Button>
-                    </Box>
-
-                    {/* Divider */}
-                    <Box
-                        sx={{
-                            height: '100%',
-                            width: '1px',
-                            backgroundColor: 'grey',
-                            marginX: 2,
-                        }}
-                    />
-
-                    {/* Right Section (Lessons) */}
-                    <Box>
-                        <Typography
-                            variant="h5"
-                            sx={{
-                                textAlign: 'center',
-                                fontWeight: 'bold',
-                                padding: 2,
-                            }}
-                        >
-                            English
-                        </Typography>
-                        <Box
-                            sx={{
-                                display: 'grid',
-                                gridTemplateRows: 'repeat(5, 1fr)',
-                                width: '100%',
-                                gap: 1,
-                                backgroundColor: '#BBE5ED',
-                                padding: 2,
-                                borderRadius: 2,
-                            }}
-                        >
-                            {/* Lesson Buttons */}
-                            <Button 
-                                variant="contained" 
-                                color="primary" 
-                                onClick={() => { window.location = '/new/src/pages/api/lessons/lesson1.js'; }}
-                            >
-                                Lesson 1
-                            </Button>
-                            <Button variant="contained" color="primary">Lesson 2</Button>
-                            <Button variant="contained" color="primary">Lesson 3</Button>
-                            <Button variant="contained" color="primary">Lesson 4</Button>
-                            <Button variant="contained" color="primary">Lesson 5</Button>
-                        </Box>
-                    </Box>
-                </Box>
-            </Container>
-        </Box>
-    );
-=======
 // Fetch lessons from MongoDB on every request.
 export async function getServerSideProps(context) {
   try {
@@ -120,7 +22,6 @@ export async function getServerSideProps(context) {
       props: { lessons: [] },
     };
   }
->>>>>>> aa85a160e49896ed30ca96e45310100bb2956166
 }
 
 export default function Lessons({ lessons }) {
