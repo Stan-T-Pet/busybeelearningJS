0fb62191bda0b8e6154731435a4267756a8d353f
B00150046 <145692695+B00150046@users.noreply.github.com>
2025-03-30 19:14:54 +0100

    Reusability update
    Made lesson page reusable, built upon Lesson

--- src/server/models/Lesson.js
+++ src/server/models/Lesson.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // File: server/models/Lesson.js
 import mongoose from "mongoose";
 
@@ -11,3 +12,17 @@ const LessonSchema = new mongoose.Schema({
 
 const Lesson = mongoose.models.Lesson || mongoose.model("Lesson", LessonSchema);
 export default Lesson;
+=======
+import mongoose from "mongoose";
+
+const LessonInfoSchema = new mongoose.Schema({
+    subject: { type: String, required: true },
+    lesson: { type: String, required: true },
+    page: { type: Number, required: true },
+    info: { type: [String], required: true },
+    examples: { type: [String], required: true },
+    images: { type: [String], required: false },
+});
+
+export default mongoose.models.LessonInfo || mongoose.model("LessonInfo", LessonInfoSchema);
+>>>>>>> Stashed changes
