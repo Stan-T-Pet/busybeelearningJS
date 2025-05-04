a032eb093ba49795ae7915bf8258149e533e1a4e
Stan-T-Pet <114077038+Stan-T-Pet@users.noreply.github.com>
2025-05-05 00:49:07 +0100

    finished music player, now available globally.

--- src/server/cdn/soundMap.js
+++ src/server/cdn/soundMap.js
@@ -13,4 +13,12 @@ const soundMap = {
   fail: `${base}fail_ydimmhy.mp3`,
 };
 
+const studyTracks = {
+  "Best Song Ever! (❁´◡`❁)": "https://res.cloudinary.com/dedlpzbla/video/upload/v1746206768/never_give_up_dbqlyy.mp3",
+  "Lofi Beats (〃￣︶￣)人(￣︶￣〃)": "https://res.cloudinary.com/dedlpzbla/video/upload/v1746207610/lofi_rdqrkd.mp3",
+  "Ambient Rain |~^///^^///^^///^~|": "https://res.cloudinary.com/dedlpzbla/video/upload/v1746208595/rain_qein7k.mp3",
+  "Nature Sounds ﹏𓃗﹏𓃗﹏": "https://res.cloudinary.com/dedlpzbla/video/upload/v1746208587/forest_sswwok.mp3",
+  "Hans the goat Zimmerman ( ⓛ ω ⓛ *)":"https://res.cloudinary.com/dedlpzbla/video/upload/v1746208712/corn_chivzy.mp3"
+};
+
 export default soundMap;
