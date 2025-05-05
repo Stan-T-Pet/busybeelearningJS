960c84c9d96411612d8feceef9d89ce72d98fcac
Stan-T-Pet <114077038+Stan-T-Pet@users.noreply.github.com>
2025-05-05 02:33:29 +0100

    explicit declaration of src folder

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const { IgnorePlugin } = webpackLib;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  srcDir: "src",
+
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
@@ -11,12 +13,10 @@ const nextConfig = {
         child_process: false,
       };
 
-      // Tell Webpack to ignore the module used by @mapbox/node-pre-gyp on the client.
       config.resolve.alias["@mapbox/node-pre-gyp/lib/util/nw-pre-gyp"] = false;
 
-      // Optionally, also add an IgnorePlugin rule targeting index.html from that folder.
       config.plugins.push(
-        new IgnorePlugin({
+        new (require("webpack").IgnorePlugin)({
           resourceRegExp: /^index\.html$/,
           contextRegExp: /@mapbox\/node-pre-gyp\/lib\/util\/nw-pre-gyp/,
         })
